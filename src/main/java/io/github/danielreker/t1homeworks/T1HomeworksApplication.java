803942bac50e6593e78e1928f56cda166b75620c
DanielReker <sasha29072004@gmail.com>
2025-05-27 03:03:30 +0400

    Init

---
+++ src/main/java/io/github/danielreker/t1homeworks/T1HomeworksApplication.java
@@ -0,0 +1,13 @@
+package io.github.danielreker.t1homeworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1HomeworksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T1HomeworksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/danielreker/t1homeworks/T1HomeworksApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.danielreker.t1homeworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1HomeworksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
